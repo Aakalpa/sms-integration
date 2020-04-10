ae3d52e1b01b37f7e162d0ba3550a82e1c7c0b34
Aakalpa Aryal <aakalpa.aryal@gmail.com>
2020-04-10 09:41:17 +0545

    minor changes

--- receive/receive.py
+++ receive/receive.py
@@ -1,24 +1,37 @@
 # /usr/bin/env python
 # Download the twilio-python library from twilio.com/docs/libraries/python
-from flask import Flask, request
-
-# from twilio import twiml
-
+from flask import Flask, request, render_template
+from twilio.rest import Client
 from twilio.twiml.messaging_response import MessagingResponse
 
 app = Flask(__name__)
 
 
-# @app.route("/sms", methods=["GET", "POST"])
-# def sms():
-#     number = request.form["From"]
-#     message_body = request.form["Body"]
 
-#     # resp = twiml.Response()
-#     # resp.message("Hello {}, you said: {}".format(number, message_body))
-#     print(number, message_body)
-#     return "hello"
+@app.route("/send")
+def send():
+    return render_template("send.html")
 
+@app.route('/sendstatus',methods = ["POST"])
+def send_status():
+    account_sid = "AC803f894ec5fd72b07ff5873fc9452913"
+    auth_token = "23af0123f39cad286bfa7eb4df19029f"
+    client = Client(account_sid, auth_token)
+
+    send_ph_no = request.form.get("send_ph_no")
+    receive_ph_no = request.form.get("receive_ph_no")
+    msg = request.form.get("msg")
+
+    message = client.messages.create(
+        body= msg,
+        # from_="+15402742780",
+        from_=send_ph_no,
+        # to='+9779861059159'
+        to=receive_ph_no
+    )
+    print(message.sid)
+    sid = message.sid
+    return render_template("sendstatus.html",send_ph_no=send_ph_no,receive_ph_no=receive_ph_no,msg=msg,sid=sid)
 
 @app.route("/sms", methods=["GET", "POST"])
 def sms():
@@ -27,12 +40,11 @@ def sms():
     resp = MessagingResponse()
     # Add a message
     resp.message("Your message has been received")
-    print("data", request.data)
-    print("args", request.args)
-    print("form", request.form)
-    print("values", request.values)
 
-    return str(resp)
+    # print("form", request.form)
+    received_info = request.form
+    # return none
+    return render_template("receive.html",received_msg = received_info)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
